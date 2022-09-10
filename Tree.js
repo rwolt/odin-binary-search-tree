cdd43a2b66e90ef14b68cc66b07830bce40257d6
Raymond Wolt <rjwolt@gmail.com>
2022-09-10 14:37:48 -0500

    Add removeDuplicatesAndSort function

--- Tree.js
+++ Tree.js
@@ -2,15 +2,31 @@ const Node = require("./Node");
 
 class Tree {
   constructor(array) {
-    this.root = this.buildTree(array);
+    let tmp = removeDuplicatesAndSort(array);
+    this.root = this.buildTree(tmp, 0, tmp.length - 1);
   }
 
-  buildTree = (array) => {
-    let start = 0;
-    let end = array.length - 1;
+  buildTree = (arr, start, end) => {
+    //Base case
+    if (start > end) {
+      return null;
+    }
+    //Recursive step
     let middle = parseInt((start + end) / 2);
-    return array[middle];
+    let current = new Node(arr[middle]);
+    current.left = this.buildTree(arr, start, middle - 1);
+    current.right = this.buildTree(arr, middle + 1, end);
+    return current;
   };
 }
+
+const removeDuplicatesAndSort = (array) => {
+  // Keep only the first occurence of an item
+  let tmp = array.filter((item, index) => {
+    return array.indexOf(item) === index;
+  });
+  // Sort the de-duped array by number ascending
+  return tmp.sort((a, b) => a - b);
+};
 
 module.exports = Tree;
--- Tree.test.js
+++ Tree.test.js
@@ -5,5 +5,45 @@ it("Has root property", () => {
 });
 
 it("Root value from 1 element array", () => {
-  expect(new Tree([5])).toHaveProperty("root", 5);
+  expect(new Tree([5])).toHaveProperty("root.data", 5);
+});
+
+it("Root value from 3 element array", () => {
+  expect(new Tree([1, 5, 9])).toHaveProperty("root.data", 5);
+});
+
+it("Left value from 3 element array", () => {
+  expect(new Tree([1, 5, 9])).toHaveProperty("root.left.data", 1);
+});
+
+it("Right value from 3 element array", () => {
+  expect(new Tree([1, 5, 9])).toHaveProperty("root.right.data", 9);
+});
+
+it("Left subtree of leaf node evaluates to null", () => {
+  expect(new Tree([5, 6, 7])).toHaveProperty("root.right.left", null);
+});
+
+it("Right subtree of leaf node evaluates to null", () => {
+  expect(new Tree([5, 6, 7])).toHaveProperty("root.right.right", null);
+});
+
+it("Array with even number of elements", () => {
+  expect(new Tree([1, 2, 3, 4])).toHaveProperty("root.right.right.data", 4);
+});
+
+it("Sorted array with duplicates", () => {
+  expect(new Tree([1, 2, 3, 3, 3, 3, 3, 3, 4])).toHaveProperty("root.data", 2);
+});
+
+it("Unsorted array 1", () => {
+  expect(new Tree([4, 3, 9, 1, 8])).toHaveProperty("root.data", 4);
+});
+
+it("Unsorted array 2", () => {
+  expect(new Tree([6, 3, 4, 2, 7])).toHaveProperty("root.left.data", 2);
+});
+
+it("Unsorted array with duplicated", () => {
+  expect(new Tree([7, 1, 6, 2, 3, 5, 4])).toHaveProperty("root.data", 4);
 });
