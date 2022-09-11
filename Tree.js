9162a131c08a9d8db0363307bf6f44027b899e09
Raymond Wolt <rjwolt@gmail.com>
2022-09-11 07:48:53 -0500

    Add insert method to Tree class

--- Tree.js
+++ Tree.js
@@ -2,8 +2,12 @@ const Node = require("./Node");
 
 class Tree {
   constructor(array) {
-    let tmp = removeDuplicatesAndSort(array);
-    this.root = this.buildTree(tmp, 0, tmp.length - 1);
+    if (array) {
+      let tmp = removeDuplicatesAndSort(array);
+      this.root = this.buildTree(tmp, 0, tmp.length - 1);
+    } else {
+      this.root = null;
+    }
   }
 
   buildTree = (arr, start, end) => {
@@ -17,6 +21,27 @@ class Tree {
     current.left = this.buildTree(arr, start, middle - 1);
     current.right = this.buildTree(arr, middle + 1, end);
     return current;
+  };
+
+  insert = (value) => {
+    this.root = this.insertRec(this.root, value);
+  };
+
+  insertRec = (root, value) => {
+    //If leaf node is found, add new node and return it
+    if (root == null) {
+      root = new Node(value);
+      return root;
+    }
+    //Otherwise recur down the tree
+    if (value < root.data) {
+      // If value is less than root, search the left subtree
+      root.left = this.insertRec(root.left, value);
+      // If value is greater than root, search right subtree
+    } else if (value > root.data) {
+      root.right = this.insertRec(root.right, value);
+    }
+    return root;
   };
 }
 
--- Tree.test.js
+++ Tree.test.js
@@ -1,5 +1,9 @@
 const Tree = require("./Tree");
 
+it("Null root of BST", () => {
+  expect(new Tree([])).toHaveProperty("root", null);
+});
+
 it("Has root property", () => {
   expect(new Tree([5])).toHaveProperty("root");
 });
@@ -44,6 +48,24 @@ it("Unsorted array 2", () => {
   expect(new Tree([6, 3, 4, 2, 7])).toHaveProperty("root.left.data", 2);
 });
 
-it("Unsorted array with duplicated", () => {
+it("Unsorted array with duplicates", () => {
   expect(new Tree([7, 1, 6, 2, 3, 5, 4])).toHaveProperty("root.data", 4);
+});
+
+it("Insert a value into an empty tree", () => {
+  const bst = new Tree();
+  bst.insert(4);
+  expect(bst).toHaveProperty("root.data", 4);
+});
+
+it("Insert a value into a tree", () => {
+  const bst = new Tree([1, 2, 3]);
+  bst.insert(4);
+  expect(bst).toHaveProperty("root.right.right.data", 4);
+});
+
+it("Insert a value into a tree created from unsorted array", () => {
+  const bst = new Tree([8, 13, 10, 14, 4, 1, 3, 7, 6]);
+  bst.insert(5);
+  expect(bst).toHaveProperty("root.left.right.right.left.data", 5);
 });
